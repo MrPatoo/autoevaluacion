820c108eab8177d81a9372bb8c5450e2b3ce1550
user name <user email>
2025-06-16 20:23:01 -0600

    doc

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
 import express from "express"
 import cors from "cors"
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
 import tasksRoutes from "./src/routes/tasks.js"
 
 const app = express()
@@ -9,7 +12,17 @@ app.use(cors
     ({origin: "*",
         credentials: true,
     })
-)
+);
+
+//Traemos el archivo json
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.resolve("./doc.json"), "utf-8")
+);
+ 
+//Mostramos el archivo al ingresar a /api/docs
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
 
 app.use(express.json());
 
